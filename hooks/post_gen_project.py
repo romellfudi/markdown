c8d46fefbf4b342cb0ea54e1eb9767b14c05c5d0
Romell D.Z. 福笛 <romllz489@gmail.com>
2019-04-24 14:01:06 -0500

    replace

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,5 +12,5 @@ os.rename(os.path.join(PROJECT_DIR, MARKDOWN),
 shutil.rmtree(PROJECT_DIR)
 
 import subprocess
-_=subprocess.run('code %s' %os.path.join(PARENT_DIR, MARKDOWN))
+_=subprocess.run('code %s' %(os.path.join(PARENT_DIR, MARKDOWN)).replace(' ','\ '))
 
