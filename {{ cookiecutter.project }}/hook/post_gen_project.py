fabd04552b31458471d34e19fec378bfbaca5804
Romell D.Z. 福笛 <romllz489@gmail.com>
2019-04-24 12:29:30 -0500

    special format

--- {{ cookiecutter.project }}/hook/post_gen_project.py
+++ {{ cookiecutter.project }}/hook/post_gen_project.py
@@ -4,7 +4,7 @@ import shutil
 PROJECT_DIR = os.path.realpath(os.path.curdir)
 PARENT_DIR = os.path.dirname(PROJECT_DIR)
 
-os.rename(os.path.join(PROJECT_DIR, 'docker-compose.yaml'),
-          os.path.join(PARENT_DIR, 'docker-compose.yaml'))
+os.rename(os.path.join(PROJECT_DIR, '{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle }}.md'),
+          os.path.join(PARENT_DIR, '{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle }}.md'))
 
 shutil.rmtree(PROJECT_DIR)
