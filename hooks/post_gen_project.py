3a88e544bb3325951da6b88f99c5e8b11d4c1233
Romell D.Z. 福笛 <romllz489@gmail.com>
2019-04-24 12:48:06 -0500

    wheter create the file, so just open it

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,14 @@
 import os
 import shutil
 
+MARKDOWN = '{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle }}.md'
+
 PROJECT_DIR = os.path.realpath(os.path.curdir)
 PARENT_DIR = os.path.dirname(PROJECT_DIR)
 
-os.rename(os.path.join(PROJECT_DIR, '{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle }}.md'),
-          os.path.join(PARENT_DIR, '{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle }}.md'))
+os.rename(os.path.join(PROJECT_DIR, MARKDOWN),
+          os.path.join(PARENT_DIR, MARKDOWN))
 
 shutil.rmtree(PROJECT_DIR)
+
+_=os.system('code %s' %MARKDOWN)
