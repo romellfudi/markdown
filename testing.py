30e583dc14cecfae23e27b83cd0f10d2e15434d0
Romell D.Z. 福笛 <romllz489@gmail.com>
2020-09-01 03:15:38 -0500

    solve item #2 from issue #3

--- testing.py
+++
@@ -1,7 +0,0 @@
-import glob
-from datetime import datetime
-
-if glob.glob("%s-a-new-day.md"% datetime.today().strftime("%Y-%m-%d") ):
-    print("Test file sucessfully")
-else:
-    raise Exception("Sorry, File does not created")
--- {{ cookiecutter.project }}/{% now 'local', '%Y-%m-%d' %}-{{ cookiecutter.tittle.lower().replace(' ','-') }}.md
+++
@@ -1,9 +0,0 @@
----
-title: "{{ cookiecutter.tittle }}"
-excerpt:
-last_modified_at: {% now 'local', '%a, %d %b %Y %H:%M' %}
-tags: 
-  - "{{ cookiecutter.tag }}"
----
-
-Wellcome back
